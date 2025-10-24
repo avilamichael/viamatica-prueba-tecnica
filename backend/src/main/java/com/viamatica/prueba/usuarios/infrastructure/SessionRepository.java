0d1791c080223be6a4918185712d1af40ab8e2c4
Michael Avila <michaelavilamoreno@gmail.com>
2025-10-24 01:09:20 -0500

    Update SessionRepository.java

--- backend/src/main/java/com/viamatica/prueba/usuarios/infrastructure/SessionRepository.java
+++ backend/src/main/java/com/viamatica/prueba/usuarios/infrastructure/SessionRepository.java
@@ -1,18 +1,9 @@
 package com.viamatica.prueba.usuarios.infrastructure;
 
 import com.viamatica.prueba.usuarios.domain.entity.Session;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.NativeQuery;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Optional;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
